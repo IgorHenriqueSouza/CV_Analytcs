deeddcea3fd56b001eebb9a16ce8253c26eb55d5
raphaelBalogo <balogoraphael@gmail.com>
2020-09-25 21:30:08 -0300

    Refatoração request api

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,66 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import PageHeader from '../../components/PageHeader';
 
 import Input from '../../components/Input';
- 
 
 import './styles.css';
 
-function EnviarLogin(e: { preventDefault: () => void; }){
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'JWT fefege...'
-    }
-    
-    axios.post('https://tcc-unip-api.herokuapp.com/login', null, {
-        headers: headers
-        })
-        .then((response) => {
-            alert (response);
-        })
-        .catch((error) => {
-
-        })
-} 
-    
-
 function Login() {
-    const [login, setLogin] = useState('');
-    const [Senha, setSenha] = useState('');
-    return (
-        <div id="page-teacher-list" className="container">
-            <PageHeader title="Cv Analitcs - Login" />
-            
-        <main>
-            
-            <Input 
-                    name="logar" 
-                    label="E-mail" 
-                    value={login} 
-                    onChange={(e) => { setLogin(e.target.value) }} 
-                />
+	const [login, setLogin] = useState('');
+	const [senha, setSenha] = useState('');
 
-            <Input 
-                    name="Senha" 
-                    label="Senha"
-                    value={Senha} 
-                    onChange={(e) => { setSenha(e.target.value) }} 
-                />
+	const EnviarLogin = () => {
+		//
+		const token = btoa(`${login}:${senha}`);
 
-                <button type="submit" onClick= {EnviarLogin}>
-                    Entrar
-                </button>
-                
-            
-        </main>
-        </div>
-    )
+		const res = axios.post('https://tcc-unip-api.herokuapp.com/login', null, {
+			headers: {
+				Authorization: `Basic ${token}`,
+			},
+		});
+	};
 
-    
+	return (
+		<div id='page-teacher-list' className='container'>
+			<PageHeader title='Cv Analitcs - Login' />
 
+			<main>
+				<Input
+					name='logar'
+					label='E-mail'
+					value={login}
+					onChange={e => {
+						setLogin(e.target.value);
+					}}
+				/>
+
+				<Input
+					name='Senha'
+					label='Senha'
+					value={senha}
+					onChange={e => {
+						setSenha(e.target.value);
+					}}
+				/>
+
+				<button type='submit' onClick={EnviarLogin}>
+					Entrar
+				</button>
+			</main>
+		</div>
+	);
 }
-
-
 
 export default Login;
